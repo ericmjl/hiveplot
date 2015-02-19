3538431f2987a8ffb4ada415af6f2e3baa1206f8
Eric Ma <ericmajinglong@gmail.com>
2015-02-18 20:39:24 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3.5',
+    version='0.1.3.6',
 
     description='Hive Plots in Matplotlib.',
     long_description='Utility class for easily making hive plots in Python.',
