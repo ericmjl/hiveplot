22effa3e2824e7a2cb02d6642c7131570e184f4e
Eric Ma <ericmjl@Erics-MacBook-Pro.local>
2015-02-21 23:51:58 -0500

    Updated to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup  # Always prefer setuptools over distutils
 
 setup(name='hiveplot',
 	  long_description='A utility for making hive plots in matplotlib.',
-      version='0.1.3.9',
+      version='0.1.4',
       py_modules=['hiveplot'],
       url='https://github.com/ericmjl/hiveplot',
       author='Eric J. Ma',
