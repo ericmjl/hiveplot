bd96667702326769c0c43ac5ba61329fc6abb8bb
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-08 22:25:04 -0400

    bug fix and code cleanup in hiveplot.py, re-ran tests, all passed

--- hiveplot.py
+++ hiveplot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-from matplotlib.path import Path 
+from matplotlib.path import Path
 
 
 class HivePlot(object):
@@ -11,22 +11,22 @@ class HivePlot(object):
     a hive plot:
     - nodes:    a dictionary of nodes, in which there are at most 3 keys
                 in the dictionary, and the nodes are sorted in a
-                pre-specified order. One common grouping is by a node 
+                pre-specified order. One common grouping is by a node
                 attribute and one possible ordering is by degree centrality.
 
-    - edges:    a dictionary of {group:edgelist}, where each edgelist is a 
-                list of (u,v,d) tuples (in NetworkX style), where u and v are 
+    - edges:    a dictionary of {group:edgelist}, where each edgelist is a
+                list of (u,v,d) tuples (in NetworkX style), where u and v are
                 the nodes to join, and d are the node attributes.
 
     The user will have to pre-sort and pre-group the nodes, and pre-map
     the edge color groupings. This code will determine the positioning
     and exact drawing of the edges.
 
-    Hive plots are non-trivial to construct. These are the most important 
+    Hive plots are non-trivial to construct. These are the most important
     features one has to consider:
-    -    Grouping of nodes: 
+    -    Grouping of nodes:
         -    at most 3 groups.
-    -    Ordering of nodes: 
+    -    Ordering of nodes:
         -    must have an ordinal or continuous node attribute
     -    Cross-group edges:
         -    Undirected is easier to draw than directed.
@@ -35,19 +35,20 @@ class HivePlot(object):
         -    Requires the duplication of an axis.
     """
 
-    def __init__(self, nodes, edges, node_colormap, edge_colormap=None, \
-    	linewidth=0.5, is_directed=False, scale=10, ax=None, fig=None):
+    def __init__(self, nodes, edges, node_colormap, edge_colormap=None,
+                 linewidth=0.5, is_directed=False, scale=10, ax=None,
+                 fig=None):
         super(HivePlot, self).__init__()
-        self.nodes = nodes #dictionary of {group:[ordered_nodes] list}
-        self.edges = edges #dictionary of {group:[(u,v,d)] tuples list}
+        self.nodes = nodes  # dictionary of {group:[ordered_nodes] list}
+        self.edges = edges  # dictionary of {group:[(u,v,d)] tuples list}
         #simplified version of the edges:
-        self.is_directed = is_directed #boolean of whether graph is supposed 
-                                       #to be directed or not
-        if fig == None:
-            self.fig = plt.figure(figsize=(8,8))
+        self.is_directed = is_directed  # boolean of whether graph is supposed
+                                        # to be directed or not
+        if fig is None:
+            self.fig = plt.figure(figsize=(8, 8))
         else:
             self.fig = fig
-        if ax == None:
+        if ax is None:
             self.ax = self.fig.add_subplot(111)
         else:
             self.ax = ax
@@ -55,46 +56,46 @@ class HivePlot(object):
         self.dot_radius = self.scale / float(4)
         self.internal_radius = scale ** 2
         self.linewidth = linewidth
-        self.node_colormap = node_colormap #dictionary of node_group:color
-        self.edge_colormap = edge_colormap #dictionary of edge_group:color
+        self.node_colormap = node_colormap  # dictionary of node_group:color
+        self.edge_colormap = edge_colormap  # dictionary of edge_group:color
 
         self.major_angle = 0
         self.initialize_major_angle()
         self.minor_angle = 0
         self.initialize_minor_angle()
-        
 
     """
     Steps in graph drawing:
-    1.    Determine the number of groups. This in turn determines the number of 
+    1.  Determine the number of groups. This in turn determines the number of
         axes to draw, and the major angle between the axes.
-    
-    2.    For each group, determine whether there are edges between members of 
+
+    2.  For each group, determine whether there are edges between members of
         the same group.
-        a.    If True:
-            -    Duplicate the axis by shifting off by a minor angle.
-            -    Draw each axis line, with length proportional to number of 
+        a.  If True:
+            -   Duplicate the axis by shifting off by a minor angle.
+            -   Draw each axis line, with length proportional to number of
                 nodes in the group:
                 -    One is at major angle + minor angle
                 -    One is at major angle - minor angle
-            -    Draw in the nodes.
-        b.    If False:
-            -    Draw the axis line at the major angle.
-            -    Length of axis line is proportional to the number of nodes in 
+            -   Draw in the nodes.
+        b.  If False:
+            -   Draw the axis line at the major angle.
+            -   Length of axis line is proportional to the number of nodes in
                 the group
-            -    Draw in the nodes.
+            -   Draw in the nodes.
 
-    3.    Determine which node group is at the 0 radians position. The angles 
-        that are calculated will have to be adjusted for whether it is at 2*pi 
+    3.  Determine which node group is at the 0 radians position. The angles
+        that are calculated will have to be adjusted for whether it is at 2*pi
         radians or at 0 radians, depending on the angle differences.
-    
-    4.    For each edge, determine the radial position of the start node and end 
-        node. Compute the middle angle and the mean radius of the start and 
-        end nodes. 
+
+    4.  For each edge, determine the radial position of the start node and end
+        node. Compute the middle angle and the mean radius of the start and
+        end nodes.
     """
+
     def simplified_edges(self):
         for group, edgelist in self.edges.items():
-            for u,v,d in edgelist:
+            for u, v, d in edgelist:
                 yield (u, v)
 
     def initialize_major_angle(self):
@@ -103,7 +104,7 @@ class HivePlot(object):
         """
         num_groups = len(self.nodes.keys())
         self.major_angle = 2 * np.pi / num_groups
-    
+
     def initialize_minor_angle(self):
         """
         Computes the minor angle: 2pi radians / 3 * number of groups.
@@ -114,10 +115,11 @@ class HivePlot(object):
 
     def set_minor_angle(self, angle):
         """
-        Sets the major angle of the hive plot. I have restricted this to be 
+        Sets the major angle of the hive plot. I have restricted this to be
         less than the major angle.
         """
-        assert angle < self.major_angle, "Minor angle cannot be greater than the major angle."
+        assert angle < self.major_angle,\
+            "Minor angle cannot be greater than the major angle."
 
         self.minor_angle = angle
 
@@ -139,7 +141,8 @@ class HivePlot(object):
         return len(self.nodes[group])
 
     def has_edge_within_group(self, group):
-        assert group in self.nodes.keys(), "{0} not one of the group of nodes".format(group)
+        assert group in self.nodes.keys(),\
+            "{0} not one of the group of nodes".format(group)
         nodelist = self.nodes[group]
         for n1, n2 in self.simplified_edges():
             if n1 in nodelist and n2 in nodelist:
@@ -153,7 +156,8 @@ class HivePlot(object):
         for i, node in enumerate(nodelist):
             r = self.internal_radius + i * self.scale
             x, y = get_cartesian(r, theta)
-            circle = plt.Circle(xy=(x,y), radius=self.dot_radius, color=self.node_colormap[group], linewidth=0)
+            circle = plt.Circle(xy=(x, y), radius=self.dot_radius,
+                                color=self.node_colormap[group], linewidth=0)
             self.ax.add_patch(circle)
 
     def group_theta(self, group):
@@ -161,30 +165,25 @@ class HivePlot(object):
         Computes the theta along which a group's nodes are aligned.
         """
         for i, g in enumerate(self.nodes.keys()):
-        	if g == group:
-        		break
-        
+            if g == group:
+                break
+
         return i * self.major_angle
         # return self.nodes.keys().index(group) * self.major_angle
         
     def add_axes_and_nodes(self):
         for i, (group, nodelist) in enumerate(self.nodes.items()):
             theta = self.group_theta(group)
-            rs = np.arange(self.internal_radius, self.internal_radius + self.scale * len(nodelist))
 
             if self.has_edge_within_group(group):
                     theta = theta - self.minor_angle
-                    # self.plot_axis(rs, theta)
                     self.plot_nodes(nodelist, theta, group)
 
                     theta = theta + 2 * self.minor_angle
-                    # self.plot_axis(rs, theta)
                     self.plot_nodes(nodelist, theta, group)
 
             else:
-                # self.plot_axis(rs, theta)
                 self.plot_nodes(nodelist, theta, group)
-
 
     def find_node_group_membership(self, node):
         """
@@ -222,7 +221,8 @@ class HivePlot(object):
         end_theta = self.node_theta(n2)
 
         start_theta, end_theta = self.correct_angles(start_theta, end_theta)
-        start_theta, end_theta = self.adjust_angles(n1, start_theta, n2, end_theta)
+        start_theta, end_theta = self.adjust_angles(n1, start_theta, n2,
+                                                    end_theta)
 
         # delta_theta = np.tanh((end_radius/start_radius - np.cos(self.major_angle))/np.sin(self.major_angle))
         # if start_theta > end_theta:
@@ -248,19 +248,19 @@ class HivePlot(object):
         # middlex, middley = get_cartesian(middle_radius, middle_theta)
         endx, endy = get_cartesian(end_radius, end_theta)
 
-        verts = [(startx, starty), (middle1x, middle1y), (middle2x, middle2y), (endx, endy)]
+        verts = [(startx, starty),
+                 (middle1x, middle1y),
+                 (middle2x, middle2y),
+                 (endx, endy)]
         codes = [Path.MOVETO, Path.CURVE4, Path.CURVE4, Path.CURVE4]
 
-        # verts = [(startx, starty), (middlex, middley), (endx, endy)]
-        # codes = [Path.MOVETO, Path.CURVE4, Path.CURVE4]
-
-
         path = Path(verts, codes)
-        if self.edge_colormap == None:
+        if self.edge_colormap is None:
             edgecolor = 'black'
         else:
             edgecolor = self.edge_colormap[group]
-        patch = patches.PathPatch(path, lw=self.linewidth, facecolor='none', edgecolor=edgecolor, alpha=0.3)
+        patch = patches.PathPatch(path, lw=self.linewidth, facecolor='none',
+                                  edgecolor=edgecolor, alpha=0.3)
         self.ax.add_patch(patch)
 
     def add_edges(self):
@@ -279,39 +279,43 @@ class HivePlot(object):
 
     def adjust_angles(self, start_node, start_angle, end_node, end_angle):
         """
-        This function adjusts the start and end angles to correct for 
+        This function adjusts the start and end angles to correct for
         duplicated axes.
         """
         start_group = self.find_node_group_membership(start_node)
         end_group = self.find_node_group_membership(end_node)
 
-        start_group_idx = self.group_theta(start_group)
-        end_group_idx = self.group_theta(end_group)
-
-        if start_group_idx == 0 and end_group_idx == len(self.nodes.keys())-1:
+        if start_group == 0 and end_group == len(self.nodes.keys())-1:
             if self.has_edge_within_group(start_group):
-                start_angle = self.correct_negative_angle(start_angle - self.minor_angle)
+                start_angle = self.correct_negative_angle(start_angle -
+                                                          self.minor_angle)
             if self.has_edge_within_group(end_group):
-                end_angle = self.correct_negative_angle(end_angle + self.minor_angle)
+                end_angle = self.correct_negative_angle(end_angle +
+                                                        self.minor_angle)
 
-        elif start_group_idx == len(self.nodes.keys())-1 and end_group_idx == 0:
+        elif start_group == len(self.nodes.keys())-1 and end_group == 0:
             if self.has_edge_within_group(start_group):
-                start_angle = self.correct_negative_angle(start_angle + self.minor_angle)
+                start_angle = self.correct_negative_angle(start_angle +
+                                                          self.minor_angle)
             if self.has_edge_within_group(end_group):
-                end_angle = self.correct_negative_angle(end_angle - self.minor_angle)
+                end_angle = self.correct_negative_angle(end_angle -
+                                                        self.minor_angle)
 
-        elif start_group_idx < end_group_idx:# and (start_group_idx != 0 and end_group_idx != len(self.nodes.keys())-1):
+        elif start_group < end_group:
             if self.has_edge_within_group(end_group):
-                end_angle = self.correct_negative_angle(end_angle - self.minor_angle)
+                end_angle = self.correct_negative_angle(end_angle -
+                                                        self.minor_angle)
             if self.has_edge_within_group(start_group):
-                start_angle = self.correct_negative_angle(start_angle + self.minor_angle)
+                start_angle = self.correct_negative_angle(start_angle +
+                                                          self.minor_angle)
 
-
-        elif end_group_idx < start_group_idx:#  and (end_group_idx != 0 and start_group_idx != len(self.nodes.keys())-1):
-            if self.has_edge_within_group(start_group): 
-                start_angle = self.correct_negative_angle(start_angle - self.minor_angle)
+        elif end_group < start_group:
+            if self.has_edge_within_group(start_group):
+                start_angle = self.correct_negative_angle(start_angle -
+                                                          self.minor_angle)
             if self.has_edge_within_group(end_group):
-                end_angle = self.correct_negative_angle(end_angle + self.minor_angle)        
+                end_angle = self.correct_negative_angle(end_angle +
+                                                        self.minor_angle)
 
         return start_angle, end_angle
 
@@ -337,13 +341,6 @@ class HivePlot(object):
         if start_angle == end_angle:
             start_angle = start_angle - self.minor_angle
             end_angle = end_angle + self.minor_angle
-            
-        # # Case when end_angle < 0:
-        # if end_angle < 0:
-        #    end_angle = 2 * np.pi + end_angle
-
-        # if start_angle < 0:
-        #    start_angle = 2 * np.pi + start_angle
 
         return start_angle, end_angle
 
@@ -351,12 +348,10 @@ class HivePlot(object):
 """
 Global helper functions go here
 """
+
+
 def get_cartesian(r, theta):
     x = r*np.sin(theta)
     y = r*np.cos(theta)
 
     return x, y
-
-
-
-    
